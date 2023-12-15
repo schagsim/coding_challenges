db23512356e508aa22da137bd6ebd4357ee85a11
Simon Schagerer <schagerer.simon@inset.com>
2023-12-15 07:19:44 +0100

    Add first sketch (first completed)

--- advent-of-code/advent-of-code/Program.cs
+++ advent-of-code/advent-of-code/Program.cs
@@ -1,5 +1,8 @@
 ﻿using advent_of_code.aoc_2024.aoc_2024_01;
+using advent_of_code.aoc_2024.aoc_2024_02;
 
 //AoC202401.ExecuteFirst();
 
-AoC202401.ExecuteSecond();
+//AoC202401.ExecuteSecond();
+
+AoC202402.ExecuteFirst();
